2594e82a761d0aaaf526188192e1810810a2f09d
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-07-17 22:48:04 +1000

    update of css for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,11 @@ import React from "react";
 export default function Home() {
   return (
     <div className="home">
-      <div class="max-width">
-        <div class="home-content">
-          <div class="text-1">Welcome, my name is</div>
-          <div class="text-2">Lara Korolyova</div>
-          <div class="text-3">
-            And I am a <span>Web Developer</span>
-          </div>
+      <div class="home-content">
+        <div class="text-1">Welcome, my name is</div>
+        <div class="text-2">Lara Korolyova</div>
+        <div class="text-3">
+          And I am a <span>Web Developer</span>
         </div>
       </div>
     </div>
